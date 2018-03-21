135d9511576c043f276a449a0bc75b688d608ce7
Travis CI User <travis@example.org>
2018-03-21 14:52:48 +0000

    Documentation for panicbit/fcm-rust@4703c9a

---
+++ fcm/response/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["FcmError",""]],"struct":[["FcmResponse",""],["MessageResult",""]]});
---
+++ implementors/fcm/trait.Service.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["fcm"] = [{text:"impl <a class=\"trait\" href=\"fcm/trait.Service.html\" title=\"trait fcm::Service\">Service</a> for <a class=\"struct\" href=\"fcm/struct.Client.html\" title=\"struct fcm::Client\">Client</a>",synthetic:false,types:["fcm::client::Client"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
